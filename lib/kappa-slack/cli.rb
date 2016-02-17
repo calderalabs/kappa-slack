38b73cccae94444c366411e43d79f1754605aa6b
Eugenio Depalo <eugeniodepalo@gmail.com>
2016-02-17 21:55:51 +0000

    Make Slack credentials required

--- lib/kappa-slack/cli.rb
+++ lib/kappa-slack/cli.rb
@@ -2,11 +2,11 @@ require 'thor'
 
 module KappaSlack
   class CLI < Thor::Group
-    class_option :slack_email, default: ENV['SLACK_EMAIL'], type: :string
-    class_option :slack_password, default: ENV['SLACK_PASSWORD'], type: :string
-    class_option :slack_team_name, default: ENV['SLACK_TEAM_NAME'], type: :string
-    class_option :skip_bttv_emotes, required: false, type: :boolean, default: false
-    class_option :skip_one_letter_emotes, required: false, type: :boolean, default: true
+    class_option :slack_email, default: ENV['SLACK_EMAIL'], required: true, type: :string
+    class_option :slack_password, default: ENV['SLACK_PASSWORD'], required: true, type: :string
+    class_option :slack_team_name, default: ENV['SLACK_TEAM_NAME'], required: true, type: :string
+    class_option :skip_bttv_emotes, type: :boolean, default: false
+    class_option :skip_one_letter_emotes, type: :boolean, default: true
 
     def self.banner
       'kappa-slack [options]'
